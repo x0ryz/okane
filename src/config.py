a66e0ea8d7365e7974a8321c51197a15468e7b2c
Bohdan <x0ryz@proton.me>
2025-10-14 23:46:12 +0300

    feat: add Redis connection to FastAPI via lifespan

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
 from pydantic_settings import BaseSettings
+from pydantic import BaseModel
+
+class RedisConfig(BaseModel):
+    host: str
+    port: int
 
 class Settings(BaseSettings):
     DATABASE_URL: str
@@ -6,6 +11,13 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    @property
+    def redis(self) -> RedisConfig:
+        return RedisConfig(host=self.REDIS_HOST, port=self.REDIS_PORT)
 
     class Config:
         env_file = ".env"
--- src/main.py
+++ src/main.py
@@ -1,10 +1,38 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from redis.asyncio import Redis
 
 from src.auth.routes import router as auth_router
 from src.transactions.routes import router as transactions_router
 from src.categories.routes import router as categories_router
+from src.config import settings
 
-app = FastAPI(title="Okane API")
+logger = logging.getLogger("uvicorn")
+
+async def setup_redis_client():
+    redis_client = Redis(
+        host=settings.redis.host,
+        port=settings.redis.port,
+        decode_responses=True
+    )
+
+    try:
+        await redis_client.ping()
+        logger.info("Redis is connected succesfully.")
+    except Exception as e:
+        logger.error(f"Redis is not connected {e}.")
+
+    return redis_client
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.redis_client = await setup_redis_client()
+    yield
+    await app.state.redis_client.close()
+
+app = FastAPI(title="Okane API", lifespan=lifespan)
 
 app.include_router(auth_router)
 app.include_router(transactions_router)
